a7043b30fffe5941930cb0b90be93e2948a86a65
Bailey Am Rhein (Syring) <153099346+baileymamrhein@users.noreply.github.com>
2023-12-07 09:36:04 -0600

    Temperature Conversions (F/C)
    Practicing creating functions and concatenating different variables.

---
+++ TemperatureConversions.py
@@ -0,0 +1,34 @@
+#Temperature conversions
+
+#def defines a function. Here, the functions purpose is to convert celsius to farenheit
+
+getInput = input("Do you want to convert Farenheit to Celsius?")
+
+if getInput=="yes":
+        getNumber = input("Please enter a numerical value: ")
+        # convert input from str to int
+        getNumber = int(getNumber)
+        # the formula to convert celsius to farenheit is x-32 * (5/9)
+        convertedNumber=(round((getNumber-32) * (5/9)))
+
+        # string the output together
+        temperature = " degrees farenheit is " + str(convertedNumber) + " degrees in celsius."
+        print(str(getNumber) + temperature)
+        input("Press 'enter' to exit.")
+        exit
+
+if getInput=="no":
+        getNumber = input("Please enter a numerical value: ")
+        # convert input from str to int
+        getNumber = int(getNumber)
+        # the formula to convert celsius to farenheit is (9/5) + 32
+        convertedNumber=(round((getNumber*(9/5))+32))
+
+        # string the output together
+        temperature = " degrees celsius is " + str(convertedNumber) + " degrees in farenheit."
+        print(str(getNumber) + temperature)
+        input("Press 'enter' to exit.")
+        exit
+else:
+    print("Sorry, please answer yes or no!")
+    input("Press 'enter' to exit and try again.")
